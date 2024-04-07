986afaa7ba6923e52ca4250305201f6775186f4a
cWilli008 <151550442+cWilli008@users.noreply.github.com>
2024-04-06 21:37:08 -0400

    Add files via upload
    System.out.println "Hello World"
    
    - 1st code

---
+++ EggMan/src/eggman.java
@@ -0,0 +1,63 @@
+
+public class eggman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t\tHello World!");
+		System.out.println("This is my first Java Program.");
+		
+		
+		
+		
+		
+		System.out.println("  -------");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("\\          /");
+		System.out.println(" \\        /");
+		System.out.println("  --------");
+	
+		System.out.println("   ");
+		
+	
+		
+		
+		
+		
+		
+		System.out.println("\\         /");
+		System.out.println(" \\       /");
+		System.out.println("   ------");
+		
+		System.out.println("   ");
+
+
+		System.out.println("+--------+");
+		
+		System.out.println("   ");
+
+		System.out.println("   -------");
+		System.out.println("  /       \\");
+		System.out.println(" /   STOP  \\");
+		System.out.println(" \\         /");
+		System.out.println("  \\       /");
+		System.out.println("   -------");
+		
+		System.out.println("   ");
+			
+		System.out.println("   ___________");
+		System.out.println("  /           \\");
+		System.out.println(" /             \\");
+		System.out.println(" +-------------+");
+		
+		System.out.println("   ");
+
+		
+		System.out.println("\t\tGo Hatters!!");
+		System.out.println("\t\tProgrammed by Cimone Williams");
+
+	
+		
+	}
+
+}
