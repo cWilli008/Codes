f87c1a158d09d8f562ca6f6a41b8aea08f3d8cf5
cWilli008 <151550442+cWilli008@users.noreply.github.com>
2024-04-06 21:50:12 -0400

    Add files via upload
    Array of tempatures
    - System.out.println --> specif number

---
+++ tempPractice/src/tempPractice/temp.java
@@ -0,0 +1,31 @@
+package tempPractice;
+
+public class temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		//Array[]
+		int [] tempature = new int[7];
+		tempature[0] = 45;
+		tempature[1] = 44;
+		tempature[2] = 39;
+		tempature[3] = 48;
+		tempature[4] = 37;
+		tempature[5] = 46;
+		tempature[6] = 53;
+		
+		
+		
+		//print certin #
+		System.out.println(tempature[3]);
+		
+		
+		
+				
+		
+		
+
+	}
+
+}
