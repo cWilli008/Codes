0c1905241a514ab1a0ea2175fd2d8957352d2b25
cWilli008 <151550442+cWilli008@users.noreply.github.com>
2024-04-08 15:46:16 -0400

    Add files via upload
    array practices

---
+++ arrayPractice/src/arrayPractice/arrays.java
@@ -0,0 +1,44 @@
+package arrayPractice;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//Example 1
+		String[] players = new String[11];
+		players[0] = "Jax";
+		players[1] = "Wally";
+		players[2] = "Rice";
+		
+		System.out.println(players[1]);
+		
+	//Example 2
+		String[] teams = {"noles", "gator", "hatters", "hurricane", "knights"};
+		System.out.println(teams[2]);
+		
+	//Example 3
+		System.out.println("All of the Flordia Football teams are: ");
+		
+		for (int i = 0; i < teams. length; i++);
+		System.out.println(teams[1]);
+		
+		
+		
+		
+				
+				
+				
+				
+				
+				
+				
+				
+
+		
+		
+		
+
+	}
+
+}
