f5c9076075dadf90d150b92f9327afe9d2ae9bb4
cWilli008 <151550442+cWilli008@users.noreply.github.com>
2024-04-10 14:05:52 -0400

    Update arrays.java

--- arrayPractice/src/arrayPractice/arrays.java
+++ arrayPractice/src/arrayPractice/arrays.java
@@ -22,6 +22,26 @@ public class arrays {
 		
 		for (int i = 0; i < teams. length; i++);
 		System.out.println(teams[1]);
+
+
+	//example 3 4/10/24
+		public static void main(String[] args) {
+
+
+        String[] fruit = new String[6];
+        fruit[0] = "mango";
+        fruit[1] = "apple";
+        fruit[2] = "pineapple";
+        fruit[3] = "cherry";
+
+
+        System.out.println(fruit[3]);
+			
+//print the repeated sring: apple 6x 
+        for (int i = 0; i < fruit.length; i++) {   
+            System.out.println(fruit[1]);
+
+
 		
 		
 		
